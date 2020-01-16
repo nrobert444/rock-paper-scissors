5e9b1d4abb6c91afb2efce9f16833530fb097a30
Nicholas Roberto <njr444@gmail.com>
2020-01-15 22:53:02 -0800

    add example.test.js file and created all 9 tests.

--- test/example.test.js
+++ test/example.test.js
@@ -3,14 +3,100 @@ import { checkResults } from '../get-random-throw.js';
 
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
 
-    //Act 
-    // Call the function you're testing and set the result to a const
+//testing draw conditions
 
-    //Assert
-    // Make assertions about what is expected valid result
-    assert.equal(true, false);
+test('checkResults is a draw', function(assert) {
+   
+    const player = 'rock';
+    const computer = 'rock';
+    const result = checkResults(player, computer);
+    const expected = 'draw';
+
+    assert.equal(expected, result);
+});
+
+test('checkResults is a draw', function(assert) {
+   
+    const player = 'paper';
+    const computer = 'paper';
+    const result = checkResults(player, computer);
+    const expected = 'draw';
+
+    assert.equal(expected, result);
+});
+
+test('checkResults is a draw', function(assert) {
+   
+    const player = 'scissors';
+    const computer = 'scissors';
+    const result = checkResults(player, computer);
+    const expected = 'draw';
+
+    assert.equal(expected, result);
+});
+
+//testing win conditions
+
+test('checkResults is a win', function(assert) {
+    
+    const player = 'rock';
+    const computer = 'scissors';
+    const result = checkResults(player, computer);
+    const expected = 'win';
+
+    assert.equal(expected, result);
+});
+
+test('checkResults is a win', function(assert) {
+    
+    const player = 'paper';
+    const computer = 'rock';
+    const result = checkResults(player, computer);
+    const expected = 'win';
+
+    assert.equal(expected, result);
+});
+
+test('checkResults is a win', function(assert) {
+   
+    const player = 'scissors';
+    const computer = 'paper';
+    const result = checkResults(player, computer);
+    const expected = 'win';
+
+    assert.equal(expected, result);
+});
+
+//testing loss conditions
+
+test('checkResults is a loss', function(assert) {
+    
+    const player = 'rock';
+    const computer = 'paper';
+    const result = checkResults(player, computer);
+    const expected = 'loss';
+
+    assert.equal(expected, result);
+});
+
+
+test('checkResults is a loss', function(assert) {
+  
+    const player = 'paper';
+    const computer = 'scissors';
+    const result = checkResults(player, computer);
+    const expected = 'loss';
+
+    assert.equal(expected, result);
+});
+
+test('checkResults is a loss', function(assert) {
+   
+    const player = 'scissors';
+    const computer = 'rock';
+    const result = checkResults(player, computer);
+    const expected = 'loss';
+
+    assert.equal(expected, result);
 });
