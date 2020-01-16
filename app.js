12ebee4962fae7554841a11b146ad1d6410d5008
Nicholas Roberto <njr444@gmail.com>
2020-01-16 10:59:45 -0800

    created new js functions for hiding and displaying images. all game functionality works.

--- app.js
+++ app.js
@@ -1,55 +1,75 @@
 import { getRandomThrow, checkResults } from './get-random-throw.js';
 
-
 const playButton = document.getElementById('play-button');
 const resetButton = document.getElementById('reset-button');
 const displayWinSpan = document.getElementById('user-win');
 const displayLossSpan = document.getElementById('user-loss');
 const displayDrawSpan = document.getElementById('user-draw');
 const resultDisplay = document.getElementById('show-message');
-const player = { win: 0, loss: 0, draw: 0 };
-
-playButton.addEventListener('click', () => {
-    const computerChoice = getRandomThrow();
-    let userGuess = document.querySelector('input:checked').value;
-    const gameResult = checkResults(userGuess, computerChoice);
-    showMessage('');
-    console.log(gameResult);
-    if (gameResult === 'win') {
-        player.win++;
-        displayWinSpan.textContent = player.win;
-        showMessage('You Win!')
-        console.log(player)
-    } else if (gameResult === 'loss') {
-        player.loss++;
-        displayLossSpan.textContent = player.loss;
-        showMessage('You Lose!')
-        console.log(player)
-    } else if (gameResult === 'draw') {
-        player.draw++;
-        displayDrawSpan.textContent = player.draw;
-        console.log(player)
-    }
-});
+const resultWinImage = document.getElementById('show-win-image');
+const resultLoseImage = document.getElementById('show-lose-image');
+let win = 0;
+let loss = 0;
+let draw = 0;
 
 function showMessage(msg) {
     resultDisplay.style.visibility = 'visible';
     resultDisplay.textContent = msg
-  }
+};
 
+function showWinLossImage(result) {
+    if (result === 'win') return resultWinImage.style.display = 'inline-block';
+    else if (result === 'loss') return resultLoseImage.style.display = 'inline-block';
+    else return;
+};
+
+function hideWinLossImage() {
+    resultWinImage.style.display = 'none';
+    resultLoseImage.style.display = 'none';
+};
+
+function resetGame() {
+    displayWinSpan.textContent = "";
+    displayLossSpan.textContent = ""; 
+    displayDrawSpan.textContent = ""; 
+    win = 0;
+    loss = 0;
+    draw = 0;
+    showMessage('');
+    hideWinLossImage();
+};
+
+function outcome(result) {
+    if (result === 'win') {
+        win++;
+        displayWinSpan.textContent = win;
+        showMessage('You Win!')
+        showWinLossImage('win');
+    } else if (result === 'loss') {
+        loss++;
+        displayLossSpan.textContent = loss;
+        showMessage('You Lose!')
+        showWinLossImage('loss');
+    } else if (result === 'draw') {
+        draw++;
+        displayDrawSpan.textContent = draw;
+        hideWinLossImage();
+    }
+};
+
+function runGame() {
+    const computerChoice = getRandomThrow();
+    const userGuess = document.querySelector('input:checked').value;
+    const gameResult = checkResults(userGuess, computerChoice);
+    hideWinLossImage();
+    showMessage('');
+    outcome(gameResult);
+};
 
 resetButton.addEventListener('click', () => {
     resetGame();
 });
 
- function resetGame() {
-    displayWinSpan.textContent = "";
-    displayLossSpan.textContent = ""; 
-    displayDrawSpan.textContent = ""; 
-    player.win = 0;
-    player.lose = 0;
-    player.draw = 0;
-    showMessage('');
-    
-
- };
+playButton.addEventListener('click', () => {
+    runGame();
+});
