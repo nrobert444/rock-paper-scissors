c1e17e2931178e43098a084fa1ae093430c404ae
Nicholas Roberto <njr444@gmail.com>
2020-01-15 20:33:33 -0800

    getRandomThrow and checkResults functions now working.

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
+import { getRandomThrow, checkResults } from './get-random-throw.js';
+
+
 const playButton = document.getElementById('play-button');
 const resetButton = document.getElementById('reset-button');
-const userGuess = document.querySelector('input:checked');
 const displayWinSpan = document.getElementById('user-win');
 const displayLossSpan = document.getElementById('user-loss');
 const displayDrawSpan = document.getElementById('user-draw');
-const computerGuess = Math.round(Math.random() * 3);
+const player = {
+    win: 0,
+    loss: 0,
+    draw: 0
+};
 
-let win;
-let loss;
-let draw;
+let win = 0;
+let loss = 0;
+let draw = 0;
 
 playButton.addEventListener('click', () => {
+    const computerChoice = getRandomThrow();
+    const userGuess = document.querySelector('input:checked').value;
+    const gameResult = checkResults(userGuess, computerChoice);
+    console.log(gameResult);
+    if (gameResult === 'win') {
+        player.win++;
+        displayWinSpan.textContent = win;
+    } else if (gameResult === 'loss') {
+        player.loss++;
+        displayLossSpan.textContent = loss;
+    } else if (gameResult === 'draw') {
+        player.draw++;
+        displayDrawSpan.textContent = draw;
+    }
+    
 
 });
 
@@ -18,13 +39,26 @@ resetButton.addEventListener('click', () => {
     resetGame();
 });
 
-function resetGame() {
-    displayWinSpan.style.visibility = 'hidden';
-    displayLossSpan.style.visibility = 'hidden';
-    displayDrawSpan.style.visibility = 'hidden';
+// function resetGame() {
+//     displayWinSpan.style.visibility = 'hidden';
+//     displayLossSpan.style.visibility = 'hidden';
+//     displayDrawSpan.style.visibility = 'hidden';
+    
 
     
 
-};
+//};
 
-function 
+// function showWinLossDraw() {
+//     const gameResult = checkResults(userGuess, computerChoice);
+//     if (gameResult === 'win') {
+//     win++;
+//     displayWinSpan.textContent = win;
+//     } else if (gameResult === 'loss') {
+//         loss++;
+//         displayLossSpan.textContent = loss;
+//     } else {
+//         draw++;
+//         displayDrawSpan.textContent = draw;
+//     }
+// };
--- get-random-throw.js
+++ get-random-throw.js
@@ -1,3 +1,22 @@
-function getRandomThrow() {
+export function getRandomThrow() {
+    const randomGenerator = Math.floor(Math.random() * 3);
+      
+    if (randomGenerator === 0) {
+        return "rock";
+    }
+        else if (randomGenerator === 1) {
+        return "paper";
+    }
+        else if (randomGenerator === 2) {
+        return "scissors";
+    }
 
+};
+
+export function checkResults(player, computer) {
+    if (computer === player) return 'draw';
+    else if (computer === 'scissors' && player === 'rock') return 'win';
+    else if (computer === 'paper' && player === 'scissors') return 'win';
+    else if (computer === 'rock' && player === 'paper') return 'win';
+    else return 'loss';
 };
